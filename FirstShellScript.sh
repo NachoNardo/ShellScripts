877b43004f46ce6dce9e64ad4aaf646ff1853b98
leonardo holanda <nardoso@gmail.com>
2022-02-08 00:47:52 -0300

    My first shell script

---
+++ FirstShellScript.sh
@@ -0,0 +1,29 @@
+# This is a simple comment line :- First Shell File
+#echo "This is my first shell file"
+
+
+#echo "This is a new line"
+
+
+#echo "This is the end of file"
+
+#Trying to use user input to increment the complexity and interaction
+
+#echo "Enter your name: "
+#read name
+
+#echo "Please, enter your location"
+#read location
+
+#echo "You are " $name
+#echo "Located in " $location
+
+#Using simple comands
+
+pwd #Printing current directory
+mkdir Testing #Creating a new folder called Testing
+cd Testing #Entering the new created folder
+pwd #Printing the new current directory
+touch Test.txt #Creating a new file in the new folder
+
+
